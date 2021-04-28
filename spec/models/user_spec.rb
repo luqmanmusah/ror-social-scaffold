38f64d0939331e27f0d95322b3d5489d3964b22e
Luqman Musah <musahluqman@gmail.com>
2021-04-28 16:57:42 +0000

    Add test for post model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,26 @@ RSpec.describe User, type: :model do
       expect(user.password).to eq('123456')
     end
   end
+
+
+  describe 'ActiveRecord associations' do
+    it 'has many posts' do
+      expect { should has_many(posts) }
+    end
+    it 'has many comments' do
+      expect { should has_many(comments) }
+    end
+    it 'has many like' do
+      expect { should has_many(likes) }
+    end
+    it 'has many  confirmed friendship' do
+      expect { should has_many(confirmed_friendships) }
+    end
+    it 'has many friends' do
+      expect { should has_many(friends) }
+    end
+    it 'has many inverse_friendships' do
+      expect { should has_many(inverse_friendships).with_foreign_key }
+    end
+  end
 end
