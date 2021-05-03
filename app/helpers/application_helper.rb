36f27f9a186afb547c7f5d0942b9bf233ecd4dfd
Addis Belete <addis.bel@gmail.com>
2021-05-03 10:13:04 +0300

    remove logic from view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def menu_link_to(link_text, link_path)
-    class_name = current_page?(link_path) ? 'menu-item active' : 'menu-item'
+    class_name = current_page?(link_path) ? "menu-item active" : "menu-item"
 
     content_tag(:div, class: class_name) do
       link_to link_text, link_path
@@ -10,9 +10,25 @@ module ApplicationHelper
   def like_or_dislike_btn(post)
     like = Like.find_by(post: post, user: current_user)
     if like
-      link_to('Dislike!', post_like_path(id: like.id, post_id: post.id), method: :delete)
+      link_to("Dislike!", post_like_path(id: like.id, post_id: post.id), method: :delete)
     else
-      link_to('Like!', post_likes_path(post_id: post.id), method: :post)
+      link_to("Like!", post_likes_path(post_id: post.id), method: :post)
+    end
+  end
+
+  def add_friend(user)
+    if (signed_in? && !Friendship.reacted?(current_user.id, user.id) && current_user != user)
+      button_to "Add Friend",
+                friendships_create_path(ids: { id1: current_user.id, id2: user.id })
+    end
+  end
+
+  def un_friend(user)
+    if (user == current_user)
+      user.friends.each do |friend|
+        return (button_to "Unfriend",
+                          friendships_destroy_path(friendship_id: Friendship.find_friendship(@user.id, friend.id)))
+      end
     end
   end
 end
