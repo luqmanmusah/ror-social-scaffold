432fccab0dce879a45854cb50b79369aa397dbdc
Addis Belete <addis.bel@gmail.com>
2021-05-04 09:15:41 +0300

    fix bugs

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,20 +1,2 @@
 module UserHelper
-
-  def put_name(user)
-    user.each do |user|
-      unless Friendship.reacted?(current_user.id, user.id)
-        return user.name
-        add_friend(user)
-      end
-    end
-  end
-
-  def add_friend(user)
-    user.each do |user|
-      if signed_in? && !Friendship.reacted?(current_user.id, user.id) && current_user != user
-        return button_to 'Add Friend', friendships_create_path(ids: { id1: current_user.id, id2: user.id })
-      end
-    end
-  end
-
-
+end
