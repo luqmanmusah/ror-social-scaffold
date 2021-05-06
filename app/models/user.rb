b161dfaa918735f97cd1e95be41ffe42234c27c9
Luqman Musah <musahluqman@gmail.com>
2021-05-06 12:59:59 +0000

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendship
-  
+
   def friends
     friends_i_sent_invitation = Friendship.where(user_id: id, confirmed: true).pluck(:friend_id)
     friends_i_got_invitation = Friendship.where(friend_id: id, confirmed: true).pluck(:user_id)
